820c2a5f1f72897aecc7ab402c263bbb4a617ba1
Kushal Kumar K A <kushalkumarka54@gmail.com>
2024-04-08 16:51:34 +0530

    Code Push

---
+++ mausam/src/api.js
@@ -0,0 +1,13 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cbcc33a694msh5724e951a683a93p15054fjsnc88b1079fafb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "56fda9f4adcf6411f632b997c8525141";
